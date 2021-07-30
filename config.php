3474edcef97f83e1229649e74150ee7756d32a78
Suman Shadangi <sumanshadangi@gmail.com>
2021-07-30 22:34:37 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,9 @@
 <?php 
 
-$server = "localhost";
-$user = "root";
-$pass = "";
-$database = "portfolio";
-
+$server = "remotemysql.com";
+$user = "HRM8yK2RUy";
+$pass = "Fz4LObMf7R";
+$database = "HRM8yK2RUy";
 $conn = mysqli_connect($server, $user, $pass, $database);
 
 if (!$conn) {
